7e7733c1e06c562be266e9992d99119e65831f2e
oksanamda <oksanashelkina@gmail.com>
2020-12-13 18:09:25 +0300

    it doesn't work but...

--- web/app.py
+++ web/app.py
@@ -1,8 +1,70 @@
-from flask import Flask, render_template, url_for, jsonify, request
+from flask import Flask, render_template, request, jsonify, make_response
+import random
+import time
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
+heading = "Lorem ipsum dolor sit amet."
+
+content = """
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+Repellat inventore assumenda laboriosam, 
+obcaecati saepe pariatur atque est? Quam, molestias nisi.
+"""
+
+db = list()  # The mock database
+
+posts = 500  # num posts to generate
+
+quantity = 20  # num posts to return per request
+
+for x in range(posts):
+
+    """
+    Creates messages/posts by shuffling the heading & content 
+    to create random strings & appends to the db
+    """
+
+    heading_parts = heading.split(" ")
+    random.shuffle(heading_parts)
+
+    content_parts = content.split(" ")
+    random.shuffle(content_parts)
+
+    db.append([x, " ".join(heading_parts), " ".join(content_parts)])
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/result')
+def result():
+    return render_template('result_page.html')
+
+
+@app.route("/result/load")
+def load():
+    """ Route to return the posts """
+
+    time.sleep(0.2)  # Used to simulate delay
+
+    if request.args:
+        counter = int(request.args.get("c"))  # The 'counter' value sent in the QS
+
+        if counter == 0:
+            print(f"Returning posts 0 to {quantity}")
+            # Slice 0 -> quantity from the db
+            res = make_response(jsonify(db[0: quantity]), 200)
+
+        elif counter == posts:
+            print("No more posts")
+            res = make_response(jsonify({}), 200)
+
+        else:
+            print(f"Returning posts {counter} to {counter + quantity}")
+            # Slice counter -> quantity from the db
+            res = make_response(jsonify(db[counter: counter + quantity]), 200)
+
+    return res
